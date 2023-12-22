bda20fbae8fe10b54809cf53645e1b112ef4aaf7
quietfanatic <lewis@wall.org>
2023-12-22 15:18:29 -0800

    uni: Use pointers instead of indexes for utf conversions

--- uni/utf.cpp
+++ uni/utf.cpp
@@ -7,63 +7,64 @@ namespace uni {
  // system is a mess (more precisely, it's several messes all broken in
  // different ways).
 static usize to_utf16_buffer (char16* buffer, Str s) {
-    char16* p = buffer;
-    for (usize i = 0; i < s.size(); i++) {
-        uint8 b0 = s[i];
+    char16* out = buffer;
+    const char* end = s.end();
+    for (const char* in = s.begin(); in != end; in++) {
+        uint8 b0 = in[0];
         if (b0 < 0b1000'0000) {
-            *p++ = b0; // ASCII
+            *out++ = b0; // ASCII
             continue;
         }
         else if (b0 < 0b1100'0000) goto invalid; // Unmatched continuation
         else if (b0 < 0b1110'0000) {
-            if (s.size() - i < 2) goto invalid;  // Truncated sequence
-            uint8 b1 = s[i+1];
+            if (end - in < 2) goto invalid;  // Truncated sequence
+            uint8 b1 = in[1];
             if (b1 < 0b1000'0000 || b1 >= 0b1100'0000) goto invalid;
             uint32 c = (b0 & 0b0001'1111) << 6
                      | (b1 & 0b0011'1111);
             if (c < 0x80) goto invalid;  // Overlong sequence
-            *p++ = c;
-            i += 1;
+            *out++ = c;
+            in += 1;
             continue;
         }
         else if (b0 < 0b1111'0000) {
-            if (s.size() - i < 3) goto invalid;  // Truncated sequence
-            uint8 b1 = s[i+1];
+            if (end - in < 3) goto invalid;  // Truncated sequence
+            uint8 b1 = in[1];
             if (b1 < 0b1000'0000 || b1 >= 0b1100'0000) goto invalid;
-            uint8 b2 = s[i+2];
+            uint8 b2 = in[2];
             if (b2 < 0b1000'0000 || b2 >= 0b1100'0000) goto invalid;
             uint32 c = (b0 & 0b0000'1111) << 12
                      | (b1 & 0b0011'1111) << 6
                      | (b2 & 0b0011'1111);
             if (c < 0x800) goto invalid;  // Overlong sequence
-            *p++ = c;
-            i += 2;
+            *out++ = c;
+            in += 2;
             continue;
         }
         else if (b0 < 0b1111'1000) {
-            if (s.size() - i < 4) goto invalid; // Truncated sequence
-            uint8 b1 = s[i+1];
+            if (end - in < 4) goto invalid; // Truncated sequence
+            uint8 b1 = in[1];
             if (b1 < 0b1000'0000 || b1 >= 0b1100'0000) goto invalid;
-            uint8 b2 = s[i+2];
+            uint8 b2 = in[2];
             if (b2 < 0b1000'0000 || b2 >= 0b1100'0000) goto invalid;
-            uint8 b3 = s[i+3];
+            uint8 b3 = in[3];
             if (b3 < 0b1000'0000 || b3 >= 0b1100'0000) goto invalid;
             uint32 c = (b0 & 0b0000'0111) << 18
                      | (b1 & 0b0011'1111) << 12
                      | (b2 & 0b0011'1111) << 6
                      | (b3 & 0b0011'1111);
             if (c < 0x10000) goto invalid;  // Overlong sequence
-            *p++ = 0xd800 + ((c - 0x10000) >> 10);
-            *p++ = 0xdc00 + ((c - 0x10000) & 0x3ff);
-            i += 3;
+            *out++ = 0xd800 + ((c - 0x10000) >> 10);
+            *out++ = 0xdc00 + ((c - 0x10000) & 0x3ff);
+            in += 3;
             continue;
         }
         invalid: {
              // Pretend the byte is latin-1 and continue
-            *p++ = b0;
+            *out++ = b0;
         }
     }
-    return p - buffer;
+    return out - buffer;
 }
 
 UniqueString16 to_utf16 (Str s) noexcept {
@@ -89,44 +90,45 @@ UniqueString16 to_utf16 (Str s) noexcept {
 }
 
 static usize from_utf16_buffer (char* buffer, Str16 s) {
-    char* p = buffer;
-    for (usize i = 0; i < s.size(); i++) {
+    char* out = buffer;
+    const char16* end = s.end();
+    for (const char16* in = s.begin(); in != end; in++) {
         uint32 c;
-        uint16 u0 = s[i];
-        if (u0 < 0xd800 || u0 >= 0xdc00 || i + 1 == s.size()) {
+        uint16 u0 = in[0];
+        if (u0 < 0xd800 || u0 >= 0xdc00 || in + 1 == end) {
             c = u0;
         }
         else {
-            uint16 u1 = s[i+1];
+            uint16 u1 = in[1];
             if (u1 < 0xdc00 || u1 >= 0xe000) {
                 c = u0;
             }
             else {
                 c = (u0 - 0xd800) << 10 | (u1 - 0xdc00);
                 c += 0x10000;
-                i += 1;
+                in += 1;
             }
         }
         if (c < 0x80) {
-            *p++ = c;
+            *out++ = c;
         }
         else if (c < 0x800) {
-            *p++ = 0b1100'0000 | (c >> 6);
-            *p++ = 0b1000'0000 | (c & 0b0011'1111);
+            *out++ = 0b1100'0000 | (c >> 6);
+            *out++ = 0b1000'0000 | (c & 0b0011'1111);
         }
         else if (c < 0x10000) {
-            *p++ = 0b1110'0000 | (c >> 12);
-            *p++ = 0b1000'0000 | (c >> 6 & 0b0011'1111);
-            *p++ = 0b1000'0000 | (c & 0b0011'1111);
+            *out++ = 0b1110'0000 | (c >> 12);
+            *out++ = 0b1000'0000 | (c >> 6 & 0b0011'1111);
+            *out++ = 0b1000'0000 | (c & 0b0011'1111);
         }
         else {
-            *p++ = 0b1111'0000 | (c >> 18);
-            *p++ = 0b1000'0000 | (c >> 12 & 0b0011'1111);
-            *p++ = 0b1000'0000 | (c >> 6 & 0b0011'1111);
-            *p++ = 0b1000'0000 | (c & 0b0011'1111);
+            *out++ = 0b1111'0000 | (c >> 18);
+            *out++ = 0b1000'0000 | (c >> 12 & 0b0011'1111);
+            *out++ = 0b1000'0000 | (c >> 6 & 0b0011'1111);
+            *out++ = 0b1000'0000 | (c & 0b0011'1111);
         }
     }
-    return p - buffer;
+    return out - buffer;
 }
 
 UniqueString from_utf16 (Str16 s) noexcept {
