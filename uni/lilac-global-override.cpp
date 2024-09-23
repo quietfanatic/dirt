9580f956eb41ccaf93dff731a97da4af3cdcb6fd
quietfanatic <lewis@wall.org>
2024-09-23 15:58:08 -0700

    uni: lilac: Mark global overrides with ALWAYS_INLINE (probably doesn't matter)

--- uni/lilac-global-override.cpp
+++ uni/lilac-global-override.cpp
@@ -2,43 +2,43 @@
 
 #include <new>
 
-void* operator new (uni::usize s) noexcept {
+ALWAYS_INLINE void* operator new (uni::usize s) noexcept {
     return uni::lilac::allocate(s);
 }
-void* operator new[] (uni::usize s) noexcept {
+ALWAYS_INLINE void* operator new[] (uni::usize s) noexcept {
     return uni::lilac::allocate(s);
 }
-void* operator new (uni::usize s, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void* operator new (uni::usize s, const std::nothrow_t&) noexcept {
     return uni::lilac::allocate(s);
 }
-void* operator new[] (uni::usize s, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void* operator new[] (uni::usize s, const std::nothrow_t&) noexcept {
     return uni::lilac::allocate(s);
 }
-void operator delete (void* p) noexcept {
+ALWAYS_INLINE void operator delete (void* p) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete[] (void* p) noexcept {
+ALWAYS_INLINE void operator delete[] (void* p) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete (void* p, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void operator delete (void* p, const std::nothrow_t&) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete[] (void* p, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void operator delete[] (void* p, const std::nothrow_t&) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
  // Compiler warns if you override unsized delete without overriding sized
  // delete as well.  However, depending on the design of both the application
  // and the libraries it uses, the passed size may be incorrect, so it should be
  // ignored.
-void operator delete (void* p, uni::usize) noexcept {
+ALWAYS_INLINE void operator delete (void* p, uni::usize) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete[] (void* p, uni::usize) noexcept {
+ALWAYS_INLINE void operator delete[] (void* p, uni::usize) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete (void* p, uni::usize, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void operator delete (void* p, uni::usize, const std::nothrow_t&) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
-void operator delete[] (void* p, uni::usize, const std::nothrow_t&) noexcept {
+ALWAYS_INLINE void operator delete[] (void* p, uni::usize, const std::nothrow_t&) noexcept {
     uni::lilac::deallocate_unknown_size(p);
 }
