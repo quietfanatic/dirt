c58de83af887e4378f737a5d0360f7280642cae1
quietfanatic <lewis@wall.org>
2024-10-09 12:38:41 -0700

    ayu: Use a flat map instead of an unordered_map for type-by-name lookup

--- ayu/reflection/description.cpp
+++ ayu/reflection/description.cpp
@@ -5,6 +5,7 @@
 #include <cxxabi.h>
 #endif
 
+#include "../../uni/hash.h"
 #include "descriptors.private.h"
 #include "type.h"
 
@@ -12,8 +13,7 @@ namespace ayu {
 namespace in {
 
 struct Registry {
-    UniqueArray<const Description*> to_init;
-    std::unordered_map<Str, const Description*> by_name;
+    UniqueArray<Pair<usize, const Description*>> by_name;
     bool initted = false;
 };
 
@@ -27,8 +27,19 @@ static void init_names () {
     if (!r.initted) {
         r.initted = true;
         plog("init types begin");
-        r.to_init.consume([&r](const Description* desc){
-            r.by_name.emplace(get_description_name(desc), desc);
+        for (auto& p : r.by_name) {
+            p.first = uni::hash(get_description_name(p.second));
+        }
+        std::sort(r.by_name.begin(), r.by_name.end(), [](auto& a, auto& b){
+            if (a.first == b.first) [[unlikely]] {
+                auto an = get_description_name(a.second);
+                auto bn = get_description_name(b.second);
+                if (an.size() == bn.size()) {
+                    return std::memcmp(an.data(), bn.data(), an.size()) < 0;
+                }
+                else return an.size() < bn.size();
+            }
+            else return a.first < b.first;
         });
         plog("init types end");
     }
@@ -36,17 +47,33 @@ static void init_names () {
 
 const Description* register_description (const Description* desc) noexcept {
     require(!registry().initted);
-    registry().to_init.push_back(desc);
+    registry().by_name.emplace_back(0, desc);
     return desc;
 }
 
 const Description* get_description_for_name (Str name) noexcept {
     init_names();
     auto& ds = registry().by_name;
-    auto iter = ds.find(name);
-    if (iter != ds.end()) return iter->second;
-    else return null;
+    auto h = uni::hash(name);
+    auto bottom = ds.begin();
+    auto top = ds.end();
+    while (bottom != top) {
+        auto mid = bottom + (top - bottom) / 2;
+        if (mid->first == h) [[unlikely]] {
+            Str n = get_description_name(mid->second);
+            if (n.size() == name.size()) [[likely]] {
+                if (n == name) [[likely]] {
+                    return mid->second;
+                }
+                else (n < name ? bottom : top) = mid;
+            }
+            else (n.size() < name.size() ? bottom : top) = mid;
+        }
+        else (mid->first < h ? bottom : top) = mid;
+    }
+    return null;
 }
+
 const Description* need_description_for_name (Str name) {
     auto desc = get_description_for_name(name);
     if (desc) return desc;
