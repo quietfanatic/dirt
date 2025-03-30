6665935e18dfb027682c29d46c6922c0937d84f0
quietfanatic <lewis@wall.org>
2025-03-29 17:48:54 -0700

    uni: Add steady_clock

--- uni/time.h
+++ uni/time.h
@@ -4,9 +4,16 @@
 
 namespace uni {
 
-inline double now () {
+inline double system_clock () {
     using namespace std::chrono;
     return duration<double>(system_clock::now().time_since_epoch()).count();
+}
+
+inline double now () { return system_clock(); }
+
+inline double steady_clock () {
+    using namespace std::chrono;
+    return duration<double>(steady_clock::now().time_since_epoch()).count();
 }
 
 } // namespace uni
