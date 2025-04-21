e1a5ee276588b7a617c632f83b607afb1bf9e06a
quietfanatic <lewis@wall.org>
2025-04-21 11:31:35 -0700

    uni: Add Indestructible<T> (skips destructor)

---
+++ uni/indestructible.h
@@ -0,0 +1,15 @@
+#include <utility>
+
+namespace uni {
+
+template <class T>
+union Indestructible {
+    T v;
+    template <class... Args>
+    constexpr Indestructible (Args&&... args) : v(std::forward<Args>(args)...) { }
+    constexpr T& operator* () { return v; }
+    constexpr T* operator-> () { return &v; }
+    constexpr ~Indestructible () { }
+};
+
+} // uni
